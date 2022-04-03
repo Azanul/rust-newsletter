ad2b886ec29bed8a4ff525f6318327154aefefee
Azanul <azanulhaque@gmail.com>
2022-04-03 04:59:02 +0000

    POST subscriber

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
+use rust_newsletter::configuration::get_configuration;
 use rust_newsletter::startup::run;
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[actix_web::main]
 
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
-    run(listener)?.await
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
+    // We have removed the hard-coded `8000` - it's now coming from our settings!
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let listener = TcpListener::bind(address)?;
+
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
+    run(listener, connection_pool)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,12 +1,20 @@
+use rust_newsletter::configuration::get_configuration;
+use sqlx::PgPool;
+
+pub struct TestApp {
+    pub address: String,
+    pub db_pool: PgPool,
+}
+
 #[actix_rt::test]
 
 async fn health_check_works() {
-    let app_address = spawn_app();
+    let app = spawn_app().await;
 
     let client = reqwest::Client::new();
 
     let response = client
-        .get(&format!("{}/health_check", &app_address))
+        .get(&format!("{}/health_check", app.address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -19,14 +27,14 @@ async fn health_check_works() {
 #[actix_rt::test]
 
 async fn subscribe_returns_a_200_for_valid_form_data() {
-    let app_address = spawn_app();
+    let app = spawn_app().await;
 
     let client = reqwest::Client::new();
 
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
     let response = client
-        .post(&format!("{}/subscriptions", &app_address))
+        .post(&format!("{}/subscriptions", app.address))
         .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
@@ -34,12 +42,21 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .expect("Failed to execute request.");
 
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+
+    assert_eq!(saved.name, "le guin");
 }
 
 #[actix_rt::test]
 
 async fn subscribe_returns_a_400_for_missing_form_data() {
-    let app_address = spawn_app();
+    let app = spawn_app().await;
 
     let client = reqwest::Client::new();
 
@@ -51,7 +68,7 @@ async fn subscribe_returns_a_400_for_missing_form_data() {
 
     for (invalid_body, error_message) in test_cases {
         let response = client
-            .post(&format!("{}/subscriptions", &app_address))
+            .post(&format!("{}/subscriptions", app.address))
             .header("Content-Type", "application/x-www-form-urlencoded")
             .body(invalid_body)
             .send()
@@ -68,12 +85,28 @@ async fn subscribe_returns_a_400_for_missing_form_data() {
 }
 
 use std::net::TcpListener;
-fn spawn_app() -> String {
+async fn spawn_app() -> TestApp {
+    let configuration = get_configuration().expect("Failed to read configuration");
+
+    let connection_string = configuration.database.connection_string();
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
 
+    let connection_pool = PgPool::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
+
     let port = listener.local_addr().unwrap().port();
-    let server = rust_newsletter::startup::run(listener).expect("Failed to bind address");
+
+    let server = rust_newsletter::startup::run(listener, connection_pool.clone())
+        .expect("Failed to bind address");
 
     let _ = tokio::spawn(server);
-    format!("http://127.0.0.1:{}", port)
+
+    let address = format!("http://127.0.0.1:{}", port);
+
+    TestApp {
+        address,
+        db_pool: connection_pool,
+    }
 }
